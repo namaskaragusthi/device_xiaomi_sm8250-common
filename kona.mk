028b599ecca0f505eeee2c738400edf81465bdba
Sena <82709617+namaskaragusthi@users.noreply.github.com>
2022-04-04 02:10:59 +0700

    sm8250-common: Don't copy com.dsi.ant.antradio_library.xml.
    Signed-off-by: namaskaragusthi <senaoutsider123@gmail.com>

--- kona.mk
+++ kona.mk
@@ -96,9 +96,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # ART
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
